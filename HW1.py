e5fd93069458ef09a6218d1051346744039e5943
107060009 <kklloo36916@gmail.com>
2020-03-17 21:53:01 +0800

    Add HW1

---
+++ HW1.py
@@ -0,0 +1,151 @@
+# Part. 1
+
+#=======================================
+
+# Import module
+
+#  csv -- fileIO operation
+
+import csv
+
+#=======================================
+
+
+# Part. 2
+
+#=======================================
+
+# Read cwb weather data
+
+cwb_filename = '107060009.csv'
+
+data = []
+
+header = []
+
+with open(cwb_filename) as csvfile:
+
+   mycsv = csv.DictReader(csvfile)
+
+   header = mycsv.fieldnames
+
+   for row in mycsv:
+
+      data.append(row)
+
+ans = []
+
+#--------------------------------------------------------------------------------- 1
+target_data = list(filter(lambda item: item['station_id'] == 'C0A880', data))
+
+length = len(target_data)
+
+max = float(target_data[0]['TEMP'])
+
+for i in range(length):
+   
+   if (float(target_data[i]['TEMP'])>max):
+      max = float(target_data[i]['TEMP'])
+   else:
+      max = max
+
+if((max == -99) or (max == -999)):
+   max = 'None'
+
+ans.append(['C0A880',max])
+
+#--------------------------------------------------------------------------------- 1
+
+#--------------------------------------------------------------------------------- 2
+target_data = list(filter(lambda item: item['station_id'] == 'C0F9A0', data))
+
+length = len(target_data)
+
+max = float(target_data[0]['TEMP'])
+
+for i in range(length):
+   
+   if (float(target_data[i]['TEMP'])>max):
+      max = float(target_data[i]['TEMP'])
+   else:
+      max = max
+
+if((max == -99) or (max == -999)):
+   max = 'None'
+
+ans.append(['C0F9A0',max])
+
+#--------------------------------------------------------------------------------- 2
+
+
+
+#--------------------------------------------------------------------------------- 3
+target_data = list(filter(lambda item: item['station_id'] == 'C0G640', data))
+
+length = len(target_data)
+
+max = float(target_data[0]['TEMP'])
+
+for i in range(length):
+   
+   if (float(target_data[i]['TEMP'])>max):
+      max = float(target_data[i]['TEMP'])
+   else:
+      max = max
+
+if((max == -99) or (max == -999)):
+   max = 'None'
+
+ans.append(['C0G640',max])
+
+#--------------------------------------------------------------------------------- 3
+
+
+
+#--------------------------------------------------------------------------------- 4
+target_data = list(filter(lambda item: item['station_id'] == 'C0R190', data))
+
+length = len(target_data)
+
+max = float(target_data[0]['TEMP'])
+
+for i in range(length):
+   
+   if (float(target_data[i]['TEMP'])>max):
+      max = float(target_data[i]['TEMP'])
+   else:
+      max = max
+
+if((max == -99) or (max == -999)):
+   max = 'None'
+
+
+ans.append(['C0R190',max])
+
+
+#--------------------------------------------------------------------------------- 4
+
+
+
+#--------------------------------------------------------------------------------- 5
+target_data = list(filter(lambda item: item['station_id'] == 'C0X260', data))
+
+length = len(target_data)
+
+max = float(target_data[0]['TEMP'])
+
+for i in range(length):
+   
+   if (float(target_data[i]['TEMP'])>max):
+      max = float(target_data[i]['TEMP'])
+   else:
+      max = max
+
+if((max == -99) or (max == -999)):
+   max = 'None'
+
+
+ans.append(['C0X260',max])
+print(ans)
+
+#--------------------------------------------------------------------------------- 5
